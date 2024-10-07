7783b4fa971be6b2ee99e757458dd76d7565fbcd
seregatipich <seregatipich@outlook.com>
2024-10-07 12:19:54 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="one-two-ocr",
     version="0.1.0",
-    packages=find_packages(include=["src", "src.*"]),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         "pytesseract==0.3.10",
         "pdf2image==1.16.3",
@@ -11,10 +12,10 @@ setup(
         "opencv-python==4.5.5.64",
         "numpy>=1.21.0,<2.0.0",
     ],
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Sergei Poluektov",
+    author_email="seregatipich@outlook.com",
     description="A simple OCR library for processing images and PDFs.",
-    url="https://github.com/yourusername/ocr",
+    url="https://github.com/seregatipich/one-two-ocr",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
